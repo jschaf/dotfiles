05b1817481bab70b3ad1620ff25b4a2226fda85b
Joe Schafer <joe@jschaf.com>
2011-05-14 00:42:36 -0400

    Add some useful globals GEM_HOME, TERM, and PATH to include gems and cabal.

--- .shell-common.sh
+++ .shell-common.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
+export GEM_HOME="$HOME/.gem"
+export PATH=".:$HOME/bin:$HOME/.cabal/bin:$HOME/.gem/bin:$PATH"
+export TERM="screen-256color"
 export EDITOR="emacs"
 export WORKON_HOME="$HOME/.virtualenvs"
 export PIP_DOWNLOAD_CACHE="$HOME/.pip/cache"
