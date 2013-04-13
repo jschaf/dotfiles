b81fb6c871fb9c4b918adaf1a1a65e7155e37712
Joe Schafer <joe@jschaf.com>
2013-04-13 19:36:47 -0400

    Change main EDITOR.

--- .shell-common.sh
+++ .shell-common.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 export GEM_HOME="$HOME/.gem"
-export PATH=".:$HOME/bin:$HOME/.cabal/bin:$HOME/.gem/bin:$PATH"
+# export WMII_CONFPATH="$HOME/.wmii"
+export PATH=".:$PATH:$HOME/bin"
 export TERM="screen-256color"
-export EDITOR="emacs"
+export ALTERNATE_EDITOR="emacs"
+export EDITOR="emacsclient -a emacs"
+export VISUAL="emacsclient"
 export WORKON_HOME="$HOME/.virtualenvs"
 export PIP_DOWNLOAD_CACHE="$HOME/.pip/cache"
 export PIP_REQUIRE_VIRTUALENV=true
 export PIP_RESPECT_VIRTUALENV=true
 export VIRTUALENV_USE_DISTRIBUTE=true
-source virtualenvwrapper.sh
+# source virtualenvwrapper.sh
