86959fce52661cce584e8dd451b117b9003f708a
Joe Schafer <joe@jschaf.com>
2013-04-16 20:35:27 -0400

    Personal modifications to wmii.

--- .wmii/wmiirc_local.py
+++ .wmii/wmiirc_local.py
@@ -1,4 +1,14 @@
 import wmiirc
+from subprocess import call
+import pygmi
 
-wmii['font'] = 'xft:Consolas-16'
+pygmi.wmii['font'] = 'xft:Consolas-11'
+# text color, background, border
+pygmi.wmii['normcolors'] = '#cecece', '#1f1f1f', '#1f1f1f'
+pygmi.wmii['focuscolors'] = '#cecece', '#1f1f1f', '#6ca6cd'
+pygmi.wmii['border'] = 1
+
+wmiirc.terminal = 'wmiir', 'setsid', 'urxvt'
+
+
 
