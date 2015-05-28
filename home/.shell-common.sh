fe68982e94e0eec91224203c644cf23e187bc796
Joe Schafer <joe@jschaf.com>
2015-05-27 20:26:14 -0400

    feat: cleanup

--- home/.shell-common.sh
+++ home/.shell-common.sh
@@ -17,12 +17,13 @@ path_remove "/usr/local/bin"
 pathmunge "/usr/local/bin"
 pathmunge "/usr/local/sbin"
 pathmunge "$HOME/bin"
+pathmunge '.'
 
 export TERM="xterm-256color"
 export ALTERNATE_EDITOR="emacs"
 export EDITOR="emacsclient -a emacs"
 export VISUAL="emacsclient"
-export WORKON_HOME="$HOME/.virtualenvs"
+export WORKON_HOME="$HOME/.dotfiles/home/.virtualenvs"
 export PROJECT_HOME="$HOME/prog"
 export PIP_DOWNLOAD_CACHE="$HOME/.pip/cache"
 export PIP_REQUIRE_VIRTUALENV=true
@@ -31,4 +32,3 @@ export VIRTUALENV_USE_DISTRIBUTE=true
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 # Set CLICOLOR if you want Ansi Colors in iTerm2
 export CLICOLOR=1
-# source virtualenvwrapper.sh
