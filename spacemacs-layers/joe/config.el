06318b678554f7e3790dcef0564e7321a3e7599d
Joe Schafer <joe@jschaf.com>
2015-05-30 21:58:57 -0400

    feat: transfer over config for jinja, utf-8

--- spacemacs-layers/joe/config.el
+++ spacemacs-layers/joe/config.el
@@ -16,8 +16,44 @@
     'evil-next-visual-line)
   (define-key evil-motion-state-map (kbd "<remap> <evil-previous-line>")
     'evil-previous-visual-line)
+  (evil-set-initial-state 'dired-mode 'emacs)
 ))
 
+
+;; Follow symlinks to source controlled files without prompting.
+(setq vc-follow-symlinks t)
+
+
+(setq tramp-default-method "ssh")
+
+;; UTF-8 everywhere
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;; .dir-local.el tweaks
+;;
+;; See http://stackoverflow.com/questions/5147060/ for explanation of
+;; why we're creating a new hook.  tl;dr: wierd interaction between
+;; `set-auto-mode' and `hack-local-variables'
+(add-hook 'hack-local-variables-hook 'my:run-local-vars-mode-hook)
+(defun my:run-local-vars-mode-hook ()
+  "Hook for all major-modes after processing local variables.
+Creates a hook for all major modes.
+e.g. `python-mode-local-vars-hook',
+`emacs-lisp-mode-local-vars-hook'"
+  (run-hooks (intern (format "%s-local-vars-hook" (symbol-name major-mode)))))
+
+(defvar my:use-jinja-for-html-p nil
+  "Use `jinja2-mode' if non-nil, otherwise `html-mode'.
+Primarily for use in .dir-locals.el")
+
+(defun my:maybe-choose-jinja2-mode ()
+  (when my:use-jinja-for-html-p
+    (jinja2-mode)))
+
+(add-hook 'html-mode-local-vars-hook 'my:maybe-choose-jinja2-mode)
 
 (use-package typescript
   :init
