bf7f4ba3ad3ebeff4826d17b157749014718e8ac
Joe Schafer <joe@jschaf.com>
2015-06-30 23:44:59 -0400

    feat: add jinja2 support

--- spacemacs-layers/joe/packages.el
+++ spacemacs-layers/joe/packages.el
@@ -15,6 +15,7 @@
     auto-completion
     emacs-lisp
     evil
+    jinja2-mode
     python
     typescript
     )
@@ -23,6 +24,36 @@ which require an initialization must be listed explicitly in the list.")
 
 (defvar joe-excluded-packages '()
   "List of packages to exclude.")
+
+(defun joe/init-jinja2-mode ()
+  (use-package jinja2
+    :defer t
+    :init
+    (progn
+      (defun my-jinja2-block (id action context)
+        (insert " ")
+        (save-excursion
+          (insert " ")))
+
+      (require 'smartparens)
+      (add-to-list 'sp-navigate-consider-stringlike-sexp
+                   'jinja2-mode)
+
+      ;; Remove curly brace binding because it prevents
+      ;; a binding for Jinja constructs.
+      (sp-local-pair 'jinja2-mode "{" "}" :actions nil)
+      (sp-local-pair 'jinja2-mode "{%" "%}"
+                     :post-handlers '(:add my-jinja2-block)
+                     :trigger "jjb")
+      (sp-local-pair 'jinja2-mode "{{" "}}"
+                     :post-handlers '(:add my-jinja2-block)
+                     :trigger "jji"))
+      )
+    :config
+    (progn
+      (add-hook 'jinja2-mode-hook 'smartparens-mode)
+    )
+  )
 
 ;; For each package, define a function joe/init-<package-joe>
 ;;
