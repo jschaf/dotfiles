0290340805a6e58985fa475227dbf825fe8f8a06
Joe Schafer <joe@jschaf.com>
2015-09-07 21:20:22 -0400

    chore: spacing and doc fixes

--- spacemacs-layers/joe/packages.el
+++ spacemacs-layers/joe/packages.el
@@ -1,11 +1,5 @@
 ;;; packages.el --- joe Layer packages File for Spacemacs
 ;;
-;; Copyright (c) 2012-2014 Sylvain Benner
-;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
-;;
-;; Author: Sylvain Benner <sylvain.benner@gmail.com>
-;; URL: https://github.com/syl20bnr/spacemacs
-;;
 ;; This file is not part of GNU Emacs.
 ;;
 ;;; License: GPLv3
@@ -30,8 +24,6 @@ which require an initialization must be listed explicitly in the list.")
 (defvar joe-excluded-packages '()
   "List of packages to exclude.")
 
-
-
 (defun joe/init-jinja2-mode ()
   (use-package jinja2
     :defer t
@@ -54,13 +46,10 @@ which require an initialization must be listed explicitly in the list.")
                      :trigger "jjb")
       (sp-local-pair 'jinja2-mode "{{" "}}"
                      :post-handlers '(:add my-jinja2-block)
-                     :trigger "jji"))
-      )
+                     :trigger "jji")))
     :config
     (progn
-      (add-hook 'jinja2-mode-hook 'smartparens-mode)
-    )
-  )
+      (add-hook 'jinja2-mode-hook 'smartparens-mode)))
 
 ;; We should be able to call this in use a `use-package', but the order is
 ;; messed up. See: https://github.com/syl20bnr/spacemacs/issues/2909
