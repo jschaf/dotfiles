254be2fbed2131bd5fa42e43731de81d2ac29e8f
Joe Schafer <joe@jschaf.com>
2015-11-14 22:58:48 -0500

    emacs: remove layers included with rust

--- spacemacs-layers/joe/packages.el
+++ spacemacs-layers/joe/packages.el
@@ -13,11 +13,8 @@
     jinja2-mode
     magit
     python
-    racer
-    rust-mode
-    toml-mode
+    rust
     typescript
-;;    color-theme-solarized
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
