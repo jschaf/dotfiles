9c81434f82c67da99446b6aaeab281011e56a64e
Joe Schafer <joe@jschaf.com>
2016-01-31 11:25:38 -0500

    fix: remove deprecated PIP_DOWNLOAD_CACHE

--- home/.shell-common.sh
+++ home/.shell-common.sh
@@ -6,7 +6,6 @@ export EDITOR="emacsclient -a emacs"
 export VISUAL="emacsclient"
 export WORKON_HOME="$HOME/.dotfiles/home/.virtualenvs"
 export PROJECT_HOME="$HOME/prog"
-export PIP_DOWNLOAD_CACHE="$HOME/.pip/cache"
 # export PIP_REQUIRE_VIRTUALENV=true
 export PIP_RESPECT_VIRTUALENV=true
 export VIRTUALENV_USE_DISTRIBUTE=true
