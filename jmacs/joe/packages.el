fc87b0b8e36693ed32a6c46680a32b244fd15067
Joe Schafer <joe@jschaf.com>
2016-05-15 21:14:03 -0400

    refactor(emacs): reenable org-mode

--- jmacs/joe/packages.el
+++ jmacs/joe/packages.el
@@ -34,7 +34,7 @@
     magit
     overseer
 ;    openwith
-;    org
+    org
 ;    (org-ref :location local)
 ;    ;; (otb :location local)
 ;    persistent-scratch
@@ -195,6 +195,99 @@ which require an initialization must be listed explicitly in the list.")
     :config
     (progn
       (setq org-src-fontify-natively t)
+
+      (setq org-directory "~/Dropbox/org")
+      (setq org-default-notes-file "~/Dropbox/org/refile.org")
+
+      (setq org-agenda-files '("~/Dropbox/org/gtd.org"))
+
+      (setq org-todo-keywords
+            '((sequence "TODO(t)" "NEXT(n)"
+                        "|" "DONE(d)")
+              ;; The @/! means log a note when entering this state and log just
+              ;; a timestamp when leaving this state
+              (sequence "WAITING(w@/!)" "HOLD(h@/!)"
+                        "|" "CANCELLED(c)")))
+
+      (setq org-todo-state-tags-triggers
+            '(("CANCELLED" ("CANCELLED" . t))
+              ("WAITING" ("WAITING" . t))
+              ("HOLD" ("WAITING") ("HOLD" . t))
+              ;; done means any done state, the one's after the "|" in
+              ;; `org-todo-keywords'
+              (done ("WAITING") ("HOLD"))
+              ("TODO" ("WAITING") ("CANCELLED") ("HOLD"))
+              ("NEXT" ("WAITING") ("CANCELLED") ("HOLD"))
+              ("DONE" ("WAITING") ("CANCELLED") ("HOLD"))))
+
+      (setq org-agenda-custom-commands
+            '(("h" "Office and Home Lists"
+               ((agenda)
+                (tags-todo "work")
+                (tags-todo "home")
+                (tags-todo "comp")
+                (tags-todo "read")))
+              ("d" "Daily Action List"
+               ((agenda ""
+                        ((org-agenda-ndays 1)
+                         (org-agenda-sorting-strategy
+                          (quote ((agenda time-up priority-down tag-up) )))
+                         (org-deadline-warning-days 0)))))))
+
+      (setq org-capture-templates
+            `(("t" "todo" entry (file ,org-default-notes-file)
+               "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+
+              ("r" "respond" entry (file ,org-default-notes-file)
+               "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n"
+               :clock-in t :clock-resume t :immediate-finish t)
+
+              ("n" "note" entry (file ,org-default-notes-file)
+               "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
+
+              ("j" "Journal" entry (file+datetree "~/git/org/diary.org")
+               "* %?\n%U\n" :clock-in t :clock-resume t)
+
+              ("w" "org-protocol" entry (file ,org-default-notes-file)
+               "* TODO Review %c\n%U\n" :immediate-finish t)
+
+              ("m" "Meeting" entry (file ,org-default-notes-file)
+               "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
+
+              ("p" "Phone call" entry (file ,org-default-notes-file)
+               "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
+
+              ("h" "Habit" entry (file ,org-default-notes-file)
+               "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n")))
+
+      (defun my/remove-empty-drawer-on-clock-out ()
+        "Remove empty LOGBOOK drawers on clock out."
+        (interactive)
+        (save-excursion
+          (beginning-of-line 0)
+          (org-remove-empty-drawer-at (point))))
+
+      (add-hook 'org-clock-out-hook 'my/remove-empty-drawer-on-clock-out 'append)
+
+      ;; Don't do any normla logging if changing todo state with Shift-Right or
+      ;; shift-left.  Useful for fixing incorrect todo states.
+      (setq org-treat-S-cursor-todo-selection-as-state-change nil)
+
+      ;; (setq org-refile-targets
+      ;;       '(("gtd.org" :maxlevel . 1)
+      ;;         ("someday.org" :level . 2)))
+
+      (setq org-refile-targets
+            '((nil :maxlevel . 9) ; nil means current buffer
+              (org-agenda-files :maxlevel . 9)))
+
+      ;; Allow paths for refiling like Projects/Setup Ubuntu
+      (setq org-refile-use-outline-path t)
+
+      ;; Show all subtrees when refiling
+      (setq org-outline-path-complete-in-steps nil)
+
+      (require 'org-drill)
       (defun swift-plaques-compile (&optional force)
         "Compile the swift-plaques project.
 If FORCE is non-nil, force recompilation even if files haven't changed."
@@ -212,6 +305,7 @@ If FORCE is non-nil, force recompilation even if files haven't changed."
           (my:replace-or-add-to-alist 'org-publish-project-alist project))
         (joe/set-leader-keys
          "cs" 'swift-plaques-compile))
+
       (defun my:work-around-org-window-drill-bug ()
         "Comment out a troublesome line in `org-toggle-latex-fragment'.
 See https://bitbucket.org/eeeickythump/org-drill/issues/30 for
@@ -231,6 +325,7 @@ details."
                 (byte-compile-file org-library-location)
                 (elisp--eval-defun)
                 (message "Modified `org-toggle-latex-fragment' for `org-drill'"))))))
+
       (my:work-around-org-window-drill-bug)
       (defun my:make-org-link-cite-key-visible (&rest _)
         "Make the org-ref cite link visible in descriptive links."
