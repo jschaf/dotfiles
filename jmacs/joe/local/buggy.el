0bbc6d6ce96d37669fad8eb16c5cec6691a9a73d
Joe Schafer <jschaf@google.com>
2016-07-05 16:20:13 -0700

    feat(emacs): add buggy

---
+++ jmacs/joe/local/buggy.el
@@ -0,0 +1,61 @@
+;;; buggy.el --- bug killing process
+
+;;; Commentary:
+
+;;; Code:
+
+(eval-when-compile (require 'cl))
+
+(defvar buggy-template-path
+  "~/Google Drive/org-templates/buggy.org"
+  "Path to the org file that represents a buggy template.")
+
+(defvar buggy-slice-template-path
+  "~/Google Drive/org-templates/slice.org"
+  "Path to the org file that represents a slice template.")
+
+(defun buggy-get-file-contents-as-string (path)
+  "Return a string of the contents of PATH."
+  (with-temp-buffer
+    (insert-file-contents path)
+    (buffer-string)))
+
+(defun buggy-get-buggy-string ()
+  "Get a string of the buggy.org template."
+  (buggy-get-file-contents-as-string buggy-template-path))
+
+(defun buggy-insert-buggy ()
+  "Insert a buggy template at the current point."
+  (interactive)
+  (insert (buggy-get-buggy-string)))
+
+(defun buggy-get-slice-string ()
+  "Get a string of the buggy.org template."
+  (buggy-get-file-contents-as-string buggy-slice-template-path))
+
+(defun buggy-get-nearest-buggy-level ()
+  "Return the level of the heading of the nearest :buggy:."
+  1)
+
+(defun buggy-demote-slice-to-level (slice-string buggy-level)
+  "Make a SLICE-STRING a child of under a heading at BUGGY-LEVEL."
+  (with-temp-buffer
+    (org-mode)
+    (insert slice-string)
+    (goto-char (point-min))
+    (cl-loop for level from 0 below buggy-level
+             do
+             (org-demote-subtree))
+    (buffer-string)))
+
+(defun buggy-insert-slice ()
+  "Insert a slice template at the current point."
+  (interactive)
+  (let* ((slice (buggy-get-slice-string))
+         (demoted-slice
+          (buggy-demote-slice-to-level slice
+                                       (buggy-get-nearest-buggy-level))))
+    (insert demoted-slice)))
+
+(provide 'buggy)
+;;; buggy.el ends here
