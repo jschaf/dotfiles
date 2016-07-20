8958b7601505633e61cea5c67c0e50493bd2ae52
Joe Schafer <jschaf@google.com>
2016-07-20 15:34:53 -0700

    feat(zsh): add update-dotfile-symlinks

--- config/zsh/updater.zsh
+++ config/zsh/updater.zsh
@@ -7,10 +7,21 @@ function update-dotfiles() {
     cd -
 }
 
+function update-dotfile-symlinks() {
+    echo "$fg[white]Updating symlinks to ~/.dotfiles$reset_color"
+    if [[ "$(uname)" == 'Linux' ]]; then
+        rcup -t linux
+    else
+        rcup
+    fi
+}
+
 # Command to get this workstation synchronized.
 function open-sesame() {
     echo "$fg[white]Welcome back! Lets get you up to speed...$reset_color"
     update-dotfiles
+    echo
+    update-dotfile-symlinks
     echo
     echo "$fg[green]You're five-by-five, good-to-go.$reset_color"
 }
