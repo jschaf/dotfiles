157bb036c7dcb6ac011ec0a75548533a1cfe3188
Joe Schafer <joe@jschaf.com>
2016-07-31 02:00:24 -0700

    chore(zsh): documents test-fonts.zsh

--- config/zsh/test-fonts.zsh
+++ config/zsh/test-fonts.zsh
@@ -1,5 +1,8 @@
-# Given a decimal number start and end print all unicode codepoint.
-# If $3 is specified, it's used as the current column number.
+# Given a decimal start and end, print all unicode codepoints in the range
+# inclusively.
+#
+# Alternating lines are striped to better see fonts that break out of the
+# bounding box.  If $3 is specified, it's used as the current column number.
 function print-decimal-unicode-range() {
     local start="$1"
     local end="$2"
@@ -29,16 +32,13 @@ function print-decimal-unicode-range() {
     printf "${allChars}${reset_color}"
 }
 
-function print-unicode-range() {
-    local start="$1"
-    local end="$2"
-    local startDecimal=$((16#$start))
-    local endDecimal=$((16#$end))
-    print-decimal-unicode-range "${startDecimal}" "${endDecimal}"
-}
-
+# Given pairs of hex numbers, print all unicode chars within the range.
+# For example,
+#
+# print-unicode-ranges eb00 ef00 cd00 ce00
+#
+# will print all characters between eb00-ef00 and cd00-ce00
 function print-unicode-ranges() {
-    local args=( "$@" )
     local count=0
     for ((i=1; i<=$#; i+=2)); do
         local start="$@[i]"
@@ -50,20 +50,9 @@ function print-unicode-ranges() {
     done
 }
 
-function test-fonts-powerline() {
-    printf "Powerline fonts glyps:\n"
-    printf "\ue0b0 \u00b1 \ue0a0 \u27a6 \u2718 \u26a1 \u2699 \ue0b1 \ue0b2 \ue0b3\n"
-}
-
-
-function test-fonts-font-awesome() {
-    printf "Font Awesome glyps:\n"
-    printf "\uf2b4 \uf119 \uf1a0 \uf23b \uf087 \uf155\n"
-}
-
-function test-nerd-patched-fonts() {
+function test-fonts() {
     echo "Nerd - Pomicons:"
-    print-unicode-range e000 e00a
+    print-unicode-ranges e000 e00a
     echo; echo
 
     echo "Nerd - Powerline"
@@ -87,18 +76,11 @@ function test-nerd-patched-fonts() {
     echo; echo
 
     echo "Nerd - Octicons"
-    print-unicode-range f400 f4ae
+    print-unicode-ranges f400 f4ae
     echo; echo
 
     echo "Nerd - Font Linux"
-    print-unicode-range f300 f315
+    print-unicode-ranges f300 f315
     echo
 
-}
-
-
-function test-fonts() {
-    test-fonts-powerline
-    printf '\n'
-    test-fonts-font-awesome
 }
