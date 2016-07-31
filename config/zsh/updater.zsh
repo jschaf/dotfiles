7d027873ccc1806d5f0425371ca8ac5933b26f5d
Joe Schafer <joe@jschaf.com>
2016-07-31 11:58:18 -0700

    refactor(zsh): use function for colored text

--- config/zsh/updater.zsh
+++ config/zsh/updater.zsh
@@ -1,6 +1,17 @@
+#/bin/zsh
 
 DOTFILES_DIR=${DOTFILES_DIR:-"${HOME}/.dotfiles"}
 DOTFILES_VENDOR_DIR="${DOTFILES_DIR}/vendor"
+
+function updater-print-info() {
+    local message="$1"
+    echo "$fg[white]${message}$reset_color"
+}
+
+function updater-print-success() {
+    local message="$1"
+    echo "$fg[green]${message}$reset_color"
+}
 
 # Return 0 if not uncommited changes, return 1 otherwise.
 function git-repo-is-clean() {
@@ -8,19 +19,19 @@ function git-repo-is-clean() {
 }
 
 function update-dotfiles() {
-    echo "$fg[white]Updating ~/.dotfiles $reset_color"
+    updater-print-info "Updating ~/.dotfiles "
     pushd "${DOTFILES_DIR}"
     git-repo-is-clean
     local needsStash=$?
     if [[ "${needsStash}" -eq 1 ]]; then
-        echo "$fg[white]Stashing changes.$reset_color"
+        updater-print-info "Stashing changes."
         git stash
     else
-        echo "$fg[white]No changes to stash.$reset_color"
+        updater-print-info "No changes to stash."
     fi
     git pull origin master
     if [[ "${needsStash}" -eq 1 ]]; then
-        echo "$fg[white]Popping stash.$reset_color"
+        updater-print-info "Popping stash."
         git stash pop
     fi
     popd
@@ -54,7 +65,7 @@ function upgrade-dotfile-vendors() {
 }
 
 function update-dotfile-symlinks() {
-    echo "$fg[white]Updating symlinks to ~/.dotfiles$reset_color"
+    updater-print-info "Updating symlinks to ~/.dotfiles"
     if [[ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]]; then
         rcup -t linux
     else
@@ -63,13 +74,13 @@ function update-dotfile-symlinks() {
 }
 
 function update-current-zsh() {
-    echo "$fg[white]Updating current ZSH instance.$reset_color"
+    updater-print-info "Updating current ZSH instance."
     reload-zshrc
 }
 
 function update-current-tmux() {
     if [[ -n "${TMUX}" ]]; then
-        echo "$fg[white]Updating current Tmux instance.$reset_color"
+        updater-print-info "Updating current Tmux instance."
         tmux source-file ~/.tmux.conf
 			  tmux display-message -p "[From Tmux] Sourced .tmux.conf."
     fi
@@ -78,12 +89,12 @@ function update-current-tmux() {
 # Command to get this workstation synchronized with the latest changes dotfile
 # changes.
 function open-sesame() {
-    echo "$fg[white]Welcome back! Lets get you up to speed...$reset_color"
+    updater-print-info "Welcome back! Lets get you up to speed..."
     if which-command "open-sesame-system" > /dev/null; then
         open-sesame-system
         echo
     else
-        echo "$fg[white]No system specific function found.$reset_color"
+        updater-print-info "No system specific function found."
     fi
     update-dotfiles
     echo
@@ -92,6 +103,6 @@ function open-sesame() {
     update-current-zsh
     echo
     update-current-tmux
-    echo "$fg[green]You're five-by-five, good-to-go.$reset_color"
+    updater-print-success "  You're five-by-five, good-to-go.  "
 }
 
