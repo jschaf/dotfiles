bb0019a578eaa927db8e88aa058c78a16297caf9
Joe Schafer <joe@jschaf.com>
2016-08-25 20:30:42 -0700

    feat(emacs): add ediff keybinding to add both regions

--- layers/joe/config.el
+++ layers/joe/config.el
@@ -439,6 +439,17 @@ If there is no line number, drop back to `find-file-at-point'."
 ;; Have a period invoke auto-fill in addition to space and newline.
 (aset auto-fill-chars ?. t)
 
+(with-eval-after-load 'ediff
+  (defun ediff-copy-both-to-C ()
+    "Add region A and region B to C."
+    (interactive)
+    (ediff-copy-diff ediff-current-difference nil 'C nil
+                     (concat
+                      (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                      (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+  (defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+  (add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map))
+
 ;; (with-eval-after-load 'dired
 
 ;; (when (eq system-type 'darwin)
