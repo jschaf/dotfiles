37f2e7fb6e9f871ff4e81c630247161e470dfd9a
Joe Schafer <joe@jschaf.com>
2016-09-01 23:39:41 -0700

    refactor(zsh): more less man color to shell-common.sh

--- bashrc
+++ bashrc
@@ -69,15 +69,6 @@ bind "set show-all-if-ambiguous on"
 # case-insensitive completion
 bind "set completion-ignore-case on"
 
-# color the man pages
-export LESS_TERMCAP_mb=$'\E[01;31m'
-export LESS_TERMCAP_md=$'\E[01;31m'
-export LESS_TERMCAP_me=$'\E[0m'
-export LESS_TERMCAP_se=$'\E[0m'
-export LESS_TERMCAP_so=$'\E[01;44;33m'
-export LESS_TERMCAP_ue=$'\E[0m'
-export LESS_TERMCAP_us=$'\E[01;32m'
-
 # append to the history file, don't overwrite it
 shopt -s histappend
 
--- shell-common.sh
+++ shell-common.sh
@@ -15,3 +15,12 @@ export DOTFILES_HOME="${HOME}/.dotfiles"
 # NodeJS and NPM setup.
 export NPM_PACKAGES="${HOME}/.npm-packages"
 export NODE_PATH="${NPM_PACKAGES}/lib/node_modules:${NODE_PATH}"
+
+# Support color in the man pages.
+export LESS_TERMCAP_mb=$'\E[01;31m'
+export LESS_TERMCAP_md=$'\E[01;31m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;44;33m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[01;32m'
