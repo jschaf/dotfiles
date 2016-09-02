c14e34cdb6a6fcd3145beba736e5a8cfdedf080e
Joe Schafer <joe@jschaf.com>
2016-09-01 22:12:32 -0700

    fix(zsh): correct my-credit-card-bill

--- config/zsh/finances.zsh
+++ config/zsh/finances.zsh
@@ -2,4 +2,7 @@
 
 # Utility functions to manage finances.
 
-alias my-credit-card-bill='hl balance --begin=8 platinum'
+# The date subshell subtracts 7 days from now to get the right month because my
+# credit card is billed on the 8th.  Once it has the right month, we use the
+# year and month of that date, and pass in 08 as the day manually.
+alias my-credit-card-bill='hl balance --begin=$(date -d "-7 days" +%Y/%m/08) platinum'
