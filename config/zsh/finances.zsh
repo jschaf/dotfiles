55046fce3fc5fce2a2790ceef64a83c69e2f36e8
Joe Schafer <joe@jschaf.com>
2016-09-03 13:38:23 -0700

    feat(zsh): add alias for checking account balance

--- config/zsh/finances.zsh
+++ config/zsh/finances.zsh
@@ -6,3 +6,5 @@
 # credit card is billed on the 8th.  Once it has the right month, we use the
 # year and month of that date, and pass in 08 as the day manually.
 alias my-credit-card-bill='hl balance --begin=$(date -d "-7 days" +%Y/%m/08) platinum'
+
+alias my-check-account-balance='hl balance checking'
