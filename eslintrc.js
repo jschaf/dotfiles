3f464a972797b51c18c4870d15a6b8c527faa434
Joe Schafer <jschaf@google.com>
2016-09-19 15:20:25 -0700

    refactor(eslint): cleanup config with working es5 config

--- eslintrc.js
+++ eslintrc.js
@@ -14,7 +14,7 @@ const ERROR = 2;
 const PERSONAL_ESLINT_CONFIG = {
 
   extends: [
-    // 'eslint-config-googlejs-es5'
+    'eslint-config-googlejs-es5',
   ],
 
   parserOptions: {
@@ -34,46 +34,21 @@ const PERSONAL_ESLINT_CONFIG = {
 
   // The list of rules and options are available at
   // http://eslint.org/docs/rules/.
-  // rules: {
-  //   'comma-dangle': [ERROR, 'always-multiline'],
-  // },
-
   rules: {
 
 
-    'accessor-pairs': OFF,
-    'dot-location': [ERROR, 'property'],
-    'dot-notation': ERROR,
-    'eol-last': ERROR,
-
-    // The style guide says nothing about the great == vs === debate.
-    'eqeqeq': [OFF, 'allow-null'],
-    
-    // Disable because it doesn't intepret goog.scope as an IIFE.
-    'indent': [OFF, 2, {SwitchCase: 1, MemberExpression: 2, outerIIFEBody: 0}],
-
-    'max-len': [WARNING, 80, 4, {
-      ignoreComments: true,
-      ignoreUrls: true
-    }],
-    'no-bitwise': OFF,
-    'no-extra-bind': ERROR,
-    'no-inner-declarations': [ERROR, 'functions'],
-    'no-multi-spaces': ERROR,
-    'no-shadow': ERROR,
-    // Disable undefined warnings until we we add goog.provide and require
-    // parsing.
-    'no-undef': OFF,
-    'no-unused-expressions': OFF,
+    // The old closure style guide had 3 lines after goog.scope.
+    'no-multiple-empty-lines': [WARNING, {max: 3, maxEOF: 1, maxBOF: 0}],
+    'no-unused-expressions': ERROR,
     'no-unused-vars': [ERROR, {args: 'none'}],
-    'quotes': [ERROR, 'single', 'avoid-escape'],
     'require-jsdoc': [WARNING, {require: {FunctionDeclaration: true,
                                           MethodDefinition: true,
                                           ClassDeclaration: true,
                                          }}],
     'space-before-blocks': ERROR,
-    'space-before-function-paren': [ERROR, {anonymous: 'never', named: 'never'}],
-    // 'strict': [ERROR, 'global'],
+    'space-before-function-paren': [ERROR, {
+      anonymous: 'never', named: 'never'
+    }],
 
   },
 
