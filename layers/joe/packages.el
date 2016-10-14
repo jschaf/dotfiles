85888c97f25936ffdf4d219308a50b706d24a70d
Joe Schafer <joe@jschaf.com>
2016-10-13 21:56:58 -0700

    feat(emacs): add formatting js code in strings

--- layers/joe/packages.el
+++ layers/joe/packages.el
@@ -413,8 +413,60 @@ ARGS is only used because we use this function as advice after
         (eslint-fix-file)
         (revert-buffer t t))
 
+      (defun my:get-beginning-of-string-point ()
+        "Gets the beginning of the string at point.
+If point is not in a string or at the beginning of a string, return nil."
+        (save-excursion
+          (if (looking-at "['\"]")
+              ;; Assume we're at the beginning of a string so just return.
+              (point)
+            ;; We're not at the beginning of a string.
+            (if (not (nth 3 (syntax-ppss)))
+                ;; We're not inside a string.
+                nil
+              ;; We're inside a string.
+              (thing-at-point--beginning-of-sexp)
+              (point)))))
+
+      (defun my:js-clang-format-string (js-code)
+        "Format JS-CODE with clang-format."
+        (let ((temp-file (make-temp-file "js-clang-format")))
+          (write-region js-code nil temp-file)
+          (shell-command (format "clang-format -i %s" temp-file))
+          (prog1
+              (with-temp-buffer
+                (insert-file-contents temp-file)
+                (buffer-string))
+            (delete-file temp-file))))
+
+      (defun my:extract-string-contents ()
+        "Gets the contents of the string at point.
+If point is not in a string, return nil."
+        (save-excursion
+          (let ((string-start (my:get-beginning-of-string-point))
+                string-end)
+            (when string-start
+              (goto-char string-start)
+              (forward-sexp)
+              (setq string-end (point))
+              (buffer-substring-no-properties (1+ string-start) (1- string-end))))))
+
+      (defun my:js-format-js-code-in-string ()
+        "Replaces the current string of JS code with nicely formatted code.
+Uses template literals to support multiple lines of code."
+        (interactive)
+        (let ((string-contents (my:extract-string-contents))
+              (string-start (my:get-beginning-of-string-point)))
+          (when string-contents
+            (goto-char string-start)
+            (kill-sexp)
+            (insert (format "`\n%s`" (my:js-clang-format-string string-contents)))
+            )))
+
       (spacemacs/set-leader-keys-for-major-mode 'js2-mode
-        "rf" #'eslint-fix-file-and-revert)
+        "rf" #'eslint-fix-file-and-revert
+        "ef" #'my:js-format-js-code-in-string
+        )
 
       (setq js2-jsdoc-typed-tag-regexp
             (concat "^\\s-*\\*+\\s-*\\(@\\(?:"
