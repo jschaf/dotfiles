33b7bc9a0c492d2c3d5e7583f6055c0acf77a43a
Joe Schafer <jschaf@google.com>
2016-10-20 13:28:49 -0700

    feat(emacs): fix jump to edited files

--- layers/joe/packages.el
+++ layers/joe/packages.el
@@ -342,16 +342,19 @@ ARGS is only used because we use this function as advice after
 
       (defun my:get-files-changed-from-git-command (directory command)
         "Returns files changed from HEAD to the commit returned by COMMAND."
-        (let* ((root (projectile-project-root))
+        (let* ((projectile-root (projectile-project-root))
+               (git-root (replace-regexp-in-string
+                          "\n" ""
+                          (shell-command-to-string "git rev-parse --show-toplevel")))
                (default-directory directory)
                (changed-files (split-string
                                (shell-command-to-string command)
-                               "\n"))
+                               "\0"))
                (changed-files-no-empty (delete "" changed-files))
                (get-relative-project-file-name
                 (lambda (file)
-                  (file-relative-name (expand-file-name file directory)
-                                      root))))
+                  (file-relative-name (expand-file-name file git-root)
+                                      projectile-root))))
           (projectile-adjust-files
            (mapcar get-relative-project-file-name
                    changed-files-no-empty))))
@@ -359,17 +362,17 @@ ARGS is only used because we use this function as advice after
       (defun my:get-files-changed-from-master-in-dir (directory)
         "Returns list of files changed from master branch in DIRECTORY."
         (my:get-files-changed-from-git-command
-         directory "git diff --name-only master"))
+         directory "git diff -z --name-only master"))
 
       (defun my:get-files-changed-from-git5-sync-in-dir (directory)
         "Returns list of files changed from master branch in DIRECTORY."
         (let ((git5-sync-hash
                (shell-command-to-string
-                (concat "git log --all --max-count=1 --format='%H' "
-                        "--grep 'git5track'"))))
+                (concat "git log --max-count=1 --format='%H' "
+                        "--grep 'git5:'"))))
           (my:get-files-changed-from-git-command
            directory
-           (format "git diff --name-only %s" git5-sync-hash))))
+           (format "git diff -z --name-only %s" git5-sync-hash))))
 
       (spacemacs/set-leader-keys
         "gd" #'helm-semantic-or-imenu
