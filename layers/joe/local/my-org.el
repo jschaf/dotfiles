ae94978074643763f1191e18b8c68ab2ff5d15d2
Joe Schafer <joe@jschaf.com>
2016-11-27 11:03:18 -0800

    org: add rangereport block for time tracking by day

--- layers/joe/local/my-org.el
+++ layers/joe/local/my-org.el
@@ -1201,5 +1201,35 @@ the form 2016-10-22."
    collect date
    do (setq date (my:increment-date date))))
 
+
+(defun org-dblock-write:rangereport (params)
+  "Display day-by-day time reports."
+  (let* ((ts (plist-get params :tstart))
+         (te (plist-get params :tend))
+         (start (time-to-seconds
+                 (apply 'encode-time (org-parse-time-string ts))))
+         (end (time-to-seconds
+               (apply 'encode-time (org-parse-time-string te))))
+         day-numbers)
+    (setq params (plist-put params :tstart nil))
+    (setq params (plist-put params :end nil))
+    (while (<= start end)
+      (save-excursion
+        (insert "\n\n"
+                (format-time-string (car org-time-stamp-formats)
+                                    (seconds-to-time start))
+                "----------------\n")
+        (org-dblock-write:clocktable
+         (plist-put
+          (plist-put
+           params
+           :tstart
+           (format-time-string (car org-time-stamp-formats)
+                               (seconds-to-time start)))
+          :tend
+          (format-time-string (car org-time-stamp-formats)
+                              (seconds-to-time end))))
+        (setq start (+ 86400 start))))))
+
 (provide 'my-org)
 ;;; my-org.el ends here
