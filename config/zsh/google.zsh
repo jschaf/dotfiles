cd6a6d636b6f73380f0014c1d8c87e717a6edadb
Joe Schafer <jschaf@google.com>
2016-12-05 18:31:41 -0800

    zsh: my_get_snippets for work and mac

--- config/zsh/google.zsh
+++ config/zsh/google.zsh
@@ -6,6 +6,16 @@
 alias ash=/google/data/ro/projects/toolshed/ash.par
 
 function my_get_snippets() {
-  ssh jschaf "/google/data/ro/projects/toolshed/ash.par --critique --calendar \
-    --event_status=accepted --mark_down --pending_cls --bugs --assigned "
+  # TODO: filter pug eng, personal training
+  # S cl/ to Submitted
+  # V cl to Reviewed
+  # P cl to Sent
+  if [[ $(uname) == 'Darwin' ]]; then
+    ssh jschaf "/google/data/ro/projects/toolshed/ash.par --critique \
+      --calendar --event_status=accepted --mark_down --pending_cls --bugs \
+      --assigned "
+  else
+    /google/data/ro/projects/toolshed/ash.par --critique --calendar \
+      --event_status=accepted --mark_down --pending_cls --bugs --assigned
+  fi
 }
