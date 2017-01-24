bf7f4b37be6610730a075e059e7c0a15148a156e
Joe Schafer <jschaf@google.com>
2017-01-24 13:02:38 -0800

    zsh: tweak interfacelift-downloader with NUM_WALLPAPERS

--- bin/my-get-wallpapers.sh
+++ bin/my-get-wallpapers.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# Downloads wallpapers from interfacelift.
+#
+# Requires interfacelift-downloader from npm.  Install with:
+#    npm install -g interfacelift-downloader
 
 WALLPAPER_HOME="${HOME}/Wallpapers"
+NUM_WALLPAPERS=1000
 
 mkdir -p "${WALLPAPER_HOME}"
 
@@ -8,5 +13,6 @@ mkdir -p "${WALLPAPER_HOME}"
 set $(xrandr | grep '*' | head -1)
 DIMENSIONS="$1"
 
-echo "Downloading images to ${WALLPAPER_HOME} at ${DIMENSIONS}"
-interfacelift-downloader "${DIMENSIONS}" "${WALLPAPER_HOME}"
+echo "Downloading ${NUM_WALLPAPERS} images to ${WALLPAPER_HOME} at \
+ ${DIMENSIONS}"
+interfacelift-downloader "${DIMENSIONS}" "${WALLPAPER_HOME}" "${NUM_WALLPAPERS}"
