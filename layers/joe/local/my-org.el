03ff136130437d126cac43c1bd83dd686551b0fa
Joe Schafer <joe@jschaf.com>
2017-01-31 10:59:39 -0800

    emacs: add tlp

--- layers/joe/local/my-org.el
+++ layers/joe/local/my-org.el
@@ -1402,44 +1402,5 @@ of the code block."
   (interactive)
   (org-babel-async-execute-generic "python"))
 
-(defvar tlp:okrs
-  '("emailAddresses"
-    "rtfMigration"
-    "shipCode"
-    "jsReadability"
-    "googlejs"
-    "fastWalrus"
-    "miniProfiler"
-    "ajdMigration"
-    "allLints"
-    "ngExport"
-    "surveyRefactor"
-    "pythonReadability"
-    "es6Migration"
-    "googlejsGithub")
-  "OKR org-mode tags.")
-
-(defvar tlp:project-tags
-  `((name . "Project Tags")
-    (candidates . ,tlp:okrs)
-    (action . identity))
-  "Org mode tags for projects.")
-
-(defun tlp:choose-project-tag ()
-  "Choose a project tag."
-  (interactive)
-  (helm :sources '(tlp:project-tags)))
-
-(defun tlp:set-project-org-tag ()
-  "Sets the current headlines tag."
-  (interactive)
-  (org-toggle-tag (tlp:choose-project-tag))
-  ;; Align tags
-  (org-set-tags-command nil 'align))
-
-(spacemacs/set-leader-keys-for-major-mode 'org-mode
-  ";" 'tlp:set-project-org-tag)
-
-
 (provide 'my-org)
 ;;; my-org.el ends here
