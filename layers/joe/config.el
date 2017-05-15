af95a2fdbd09ee3f852eeba96ff1368d3f9ec1ce
Joe Schafer <joe@jschaf.com>
2017-05-14 23:35:04 -0700

    emacs: make copy-paste work in terminal emacs

--- layers/joe/config.el
+++ layers/joe/config.el
@@ -865,6 +865,39 @@ This is mainly useful for SSHFS."
     (anaconda-mode -1)
     (eldoc-mode -1)))
 
+(unless window-system
+  (defun my:paste-from-emacs-client ()
+    "Paste into a terminal Emacs."
+    (if window-system
+        (error "Trying to paste into GUI emacs.")
+      (let ((paste-data (s-trim (shell-command-to-string "clipboard-paste"))))
+        ;; When running via emacsclient, paste into the current buffer.  Without
+        ;; this, we would paste into the server buffer.
+        (with-current-buffer (window-buffer)
+          (insert paste-data))
+        ;; Add to kill-ring
+        (kill-new paste-data))))
+
+  (defun my:copy-from-terminal (text)
+    "Store text in the clipboard from a terminal Emacs."
+    (if window-system
+        (error "Trying to copy text in GUI emacs.")
+      (with-temp-buffer
+        (insert text)
+        (call-process-region (point-min) (point-max)
+                             "~/bin/clipboard-copy"))))
+
+  (defun my:buffer-substring-terminal-filter (beg end &optional delete)
+    "A filter that uses the default filter but also adds text to clipboard."
+    (let ((result (buffer-substring--filter beg end delete)))
+      ;; Only copy sizable entries to avoid unnecessary system calls.
+      (when (> (length result) 4)
+        (my:copy-from-terminal result))
+      result))
+
+  (setq-default filter-buffer-substring-function
+                #'my:buffer-substring-terminal-filter))
+
 ;; LaTeX template
 ;; Minted - http://www.ctan.org/pkg/minted
 
