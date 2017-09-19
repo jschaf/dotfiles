a5f4669f0b5f7991796747366a24e26bf18c5ed2
Joe Schafer <jschaf@google.com>
2017-09-19 16:41:06 -0700

    emacs: migrate over custom-file from spacemacs config

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -3,9 +3,105 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-revert-use-notify t)
+ '(bibtex-entry-format
+   (quote
+    (opts-or-alts required-fields numerical-fields page-dashes whitespace inherit-booktitle realign last-comma delimiters unify-case braces strings sort-fields)))
+ '(css-indent-offset 2)
+ '(cursor-in-non-selected-windows nil)
+ '(evil-search-module (quote evil-search))
+ '(evil-want-Y-yank-to-eol t)
+ '(exec-path-from-shell-check-startup-files nil)
+ '(git-commit-summary-max-length 65)
+ '(git-gutter-fr:side (quote left-fringe))
+ '(hl-todo-keyword-faces
+   (quote
+    (("HOLD" . "#d0bf8f")
+     ("TODO" . "#cc9393")
+     ("NEXT" . "#dca3a3")
+     ("THEM" . "#dc8cc3")
+     ("PROG" . "#7cb8bb")
+     ("OKAY" . "#7cb8bb")
+     ("DONT" . "#5f7f5f")
+     ("FAIL" . "#8c5353")
+     ("DONE" . "#afd8af")
+     ("NOTE" . "#d0bf8f")
+     ("KLUDGE" . "#d0bf8f")
+     ("HACK" . "#d0bf8f")
+     ("FIXME" . "#cc9393")
+     ("XXX" . "#cc9393")
+     ("XXXX" . "#cc9393")
+     ("???" . "#cc9393")
+     ("WAIT" . "#d0bf8f")
+     ("CANX" . "#d0bf8f"))))
+ '(httpd-port 31410)
+ '(indicate-buffer-boundaries (quote left))
+ '(js-doc-return-line " * @return {}
+")
+ '(js-expr-indent-offset 2)
+ '(js-indent-level 2)
+ '(js-paren-indent-offset 2)
+ '(js-switch-indent-offset 2)
+ '(js2-bounce-indent-p nil)
+ '(large-file-warning-threshold 60000000)
+ '(ledger-highlight-xact-under-point nil)
+ '(magit-diff-refine-hunk (quote all))
+ '(org-adapt-indentation nil)
+ '(org-attach-auto-tag "attach")
+ '(org-drill-add-random-noise-to-intervals-p t)
+ '(org-drill-forgetting-index 20)
+ '(org-drill-hide-item-headings-p t)
+ '(org-drill-learn-fraction 0.25)
+ '(org-drill-left-cloze-delimiter "[(")
+ '(org-drill-right-cloze-delimiter ")]")
+ '(org-drill-sm5-initial-interval 1.4)
+ '(org-drill-spaced-repetition-algorithm (quote sm5))
+ '(org-edit-src-content-indentation 0)
+ '(org-file-apps
+   (quote
+    ((auto-mode . emacs)
+     ("\\.mm\\'" . default)
+     ("\\.x?html?\\'" . default)
+     ("\\.pdf\\'" . "evince %s"))))
+ '(org-habit-preceding-days 10)
+ '(org-html-checkbox-type (quote html))
+ '(org-lowest-priority 69)
+ '(org-modules
+   (quote
+    (org-bbdb org-bibtex org-checklist org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m org-drill)))
+ '(org-pomodoro-finished-sound-p t)
+ '(org-pomodoro-format "P~%s")
+ '(org-pomodoro-play-sounds nil)
+ '(org-reverse-note-order t)
  '(package-selected-packages
    (quote
+<<<<<<< Updated upstream
     (unfill which-key use-package spacemacs-theme smex smartparens magit lorem-ipsum general f evil-visualstar evil-terminal-cursor-changer evil-surround evil-numbers evil-nerd-commenter evil-escape evil-anzu elisp-slime-nav crux counsel-projectile bind-map avy aggressive-indent))))
+=======
+    (magit which-key use-package spacemacs-theme smex lorem-ipsum general f evil-visualstar evil-terminal-cursor-changer evil-surround evil-numbers evil-nerd-commenter evil-escape evil-anzu elisp-slime-nav crux counsel-projectile bind-map avy async)))
+ '(projectile-globally-ignored-directories
+   (quote
+    ("third_party" "blaze-testlogs" "blaze-out" "blaze-google3" "blaze-genfiles" "blaze-bin" "node_modules" ".idea" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" "READONLY")))
+ '(safe-local-variable-values
+   (quote
+    ((eval progn
+           (pp-buffer)
+           (indent-buffer))
+     (org-tufte-blog-url "http://delta46.us")
+     (org-tufte-blog-directory-output "~/prog/blog/output")
+     (org-tufte-blog-directory-static "~/prog/blog/static")
+     (org-tufte-blog-directory "~/prog/blog")
+     (org-ref-bibliography-files "~/prog/blog/references/hammelburg.bib" "~/prog/blog/references/somalia.bib")
+     (auto-recompile . t))))
+ '(sh-basic-offset 2)
+ '(sh-indentation 2)
+ '(sp-highlight-pair-overlay nil)
+ '(spacemacs-theme-comment-bg nil)
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-css-indent-offset 2)
+ '(web-mode-enable-current-element-highlight t)
+ '(web-mode-markup-indent-offset 2))
+>>>>>>> Stashed changes
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
