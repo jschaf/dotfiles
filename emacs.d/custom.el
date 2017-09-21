e08466bde5f884c5025183bb7a7c53c592c3aded
Joe Schafer <jschaf@google.com>
2017-09-21 15:47:43 -0700

    emacs: add more packages to custom

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (auto-highlight-symbol evil-exchange evil-args evil-iedit-state evil-replace-with-register wgrep zeal-at-point which-key use-package unfill string-inflection spacemacs-theme smex smartparens magit lorem-ipsum js2-mode hl-todo general f evil-visualstar evil-terminal-cursor-changer evil-surround evil-numbers evil-nerd-commenter evil-escape evil-anzu elisp-slime-nav crux counsel-projectile bind-map avy aggressive-indent))))
+    (auto-highlight-symbol which-key wgrep use-package unfill string-inflection spacemacs-theme smex magit lorem-ipsum js2-mode hl-todo general f evil-visualstar evil-terminal-cursor-changer evil-surround evil-replace-with-register evil-numbers evil-nerd-commenter evil-lisp-state evil-iedit-state evil-exchange evil-escape evil-args evil-anzu elisp-slime-nav crux counsel-projectile avy aggressive-indent))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
