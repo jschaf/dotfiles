4a39c9a95ed16f788ab3d39fe7487039aed125ff
Joe Schafer <jschaf@google.com>
2017-09-23 00:38:10 -0700

    emacs: simplify new module creation

--- emacs.d/funcs/abn-funcs-emacs-config.el
+++ emacs.d/funcs/abn-funcs-emacs-config.el
@@ -7,53 +7,30 @@
 
 (require 's)
 
-(defun abn/new-module (name)
+(defun abn/new-module (name &optional directory)
   (interactive
-   (list (read-string "New module name: ")))
-  (abn//make-new-module-file name)
-  (abn//make-new-funcs-file name))
+   (list (read-string "New module name: ")
+         (or directory abn-dir)))
+  (abn//make-new-module-file name directory)
+  (abn//make-new-funcs-file name directory))
 
-(defun abn//make-new-module-file (name)
-  "Creates a new module from NAME."
-  (interactive
-   (list (read-string "New module name: ")))
-  (let* ((description (format "Config for %s" name))
-	 (full-file-name (format "abn-module-%s.el" name))
-	 (module-path (concat abn-modules-dir "/" full-file-name)))
-    (if (file-exists-p module-path)
-	(error "Module `%s' already exists.")
-      (find-file module-path)
-      (insert (abn//new-elisp-file-template "module" name description))
-      (goto-char (point-min))
-      (forward-line 9)
-      (insert
-       (s-join "\n"
-	       (list
-                (format "(use-package abn-funcs-%s" name)
-                "  :ensure nil ; local package"
-                "  :general"
-                "  (abn/define-leader-keys"
-                "   \"fem\" 'abn/new-module)"
-                ")"
-                "\n")))
-
-      (save-buffer))))
-
-(defun abn//make-new-funcs-file (name)
+(defun abn//make-new-funcs-file (name directory)
   "Creates a new module from NAME."
   (interactive
    (list (read-string "New funcs name: ")))
   (let* ((description (format "Functions for %s" name))
 	 (full-file-name (format "abn-funcs-%s.el" name))
-	 (funcs-path (concat abn-funcs-dir "/" full-file-name)))
+	 (funcs-path (concat directory "/funcs/"  full-file-name)))
     (if (file-exists-p funcs-path)
 	(error "Funcs `%s' already exists.")
       (find-file funcs-path)
-      (insert (abn//new-elisp-file-template "funcs" name description))
+      (insert (abn//new-funcs-file-template name description))
+      (goto-char (point-min))
+      (search-forward "Code:")
+      (forward-line 1)
       (save-buffer))))
 
-(defun abn//new-elisp-file-template (type name description)
-  "Returns a string of a complete emacs lisp file."
+(defun abn//new-funcs-file-template (name description)
   (s-join
    "\n"
    (list
@@ -63,9 +40,48 @@
     ";;"
     ""
     ";;; Code:"
+    ""
+    (format "(provide 'abn-%s-%s)" type name)
+    (format ";;; abn-%s-%s.el ends here" type name)
+    "" ; trailing newline
+    )))
+
+(defun abn//make-new-module-file (name directory)
+  "Creates a new module from NAME."
+  (interactive
+   (list (read-string "New module name: ")))
+  (let* ((description (format "Config for %s" name))
+	 (full-file-name (format "abn-module-%s.el" name))
+	 (module-path (concat directory "/modules/" full-file-name)))
+    (if (file-exists-p module-path)
+	(error "Module `%s' already exists.")
+      (find-file module-path)
+      (insert (abn//new-module-file-template name description))
+      (goto-char (point-min))
+      (search-forward ":ensure nil")
+      (save-buffer))))
+
+(defun abn//new-module-file-template (name description)
+  "Returns a string of a complete emacs lisp file."
+  (s-join
+   "\n"
+   (list
+    (format ";;; abn-module-%s.el --- %s" name description)
+    ""
+    ";;; Commentary:"
+    ";;"
+    ""
+    ";;; Code:"
     "(eval-when-compile"
     "  (require 'use-package))"
     ""
+    (format "(use-package abn-funcs-%s" name)
+    "  :ensure nil ; local package"
+    "  :general"
+    "  (abn/define-leader-keys"
+    "   \"fem\" 'abn/new-module)"
+    ")"
+    "\n"
     "(use-package FOO"
     "  :defer t"
     "  :diminish foo-mode"
@@ -80,8 +96,8 @@
     ""
     ")"
     ""
-    (format "(provide 'abn-%s-%s)" type name)
-    (format ";;; abn-%s-%s.el ends here" type name)
+    (format "(provide 'abn-module-%s)" name)
+    (format ";;; abn-module-%s.el ends here" name)
     "" ; trailing newline
     )))
 
