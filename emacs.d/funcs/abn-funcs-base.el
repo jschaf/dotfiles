1ae7e677e50a4d373d49858e837c362d78f15640
Joe Schafer <joe@jschaf.com>
2017-09-24 01:12:08 -0700

    emacs: add abn/stopwatch

--- emacs.d/funcs/abn-funcs-base.el
+++ emacs.d/funcs/abn-funcs-base.el
@@ -5,14 +5,11 @@
 
 ;;; Code:
 
-(defun abn/system-is-mac ()
-  (eq system-type 'darwin))
-
-(defun abn/system-is-linux ()
-  (eq system-type 'gnu/linux))
-
-(defun abn/system-is-mswindows ()
-  (eq system-type 'windows-nt))
+(defmacro abn/stop-watch (&rest forms)
+  (let ((temp-var (make-symbol "start")))
+    `(let* ((,temp-var (float-time)))
+       (progn . ,forms)
+       (- (float-time) ,temp-var))))
 
 (defun abn/set-gc-cons-threshold-to-2mb ()
   (setq gc-cons-threshold (* 2 1000 1000)))
@@ -23,6 +20,15 @@
 (defun abn/start-server-if-not-running ()
   "Start the server if it's not running."
   (unless (server-running-p) (server-start)))
+
+(defun abn/system-is-mac ()
+  (eq system-type 'darwin))
+
+(defun abn/system-is-linux ()
+  (eq system-type 'gnu/linux))
+
+(defun abn/system-is-mswindows ()
+  (eq system-type 'windows-nt))
 
 (provide 'abn-funcs-base)
 ;;; abn-funcs-base.el ends here
