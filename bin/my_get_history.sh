3658321cd1adf3d20e1a913790b176b71997ea8f
Joe Schafer <jschaf@google.com>
2017-10-09 15:41:21 -0700

    bin: add docs for my_get_history

--- bin/my_get_history.sh
+++ bin/my_get_history.sh
@@ -1,5 +1,11 @@
 #!/bin/zsh
 
 export HISTFILE="${ZDOTDIR}/.zsh_history"
+
+# Read the history from HISTFILE.
 fc -R
+
+# -l: List history on STDOUT.
+# -n: Suppress history numbers
+# -r: Reverse history so the latest entry is the first line.
 fc -lnr -10
