ef22667755df6d53a675128c3b1d0ccea928d32e
Joe Schafer <jschaf@google.com>
2017-10-10 00:43:32 -0700

    emacs: fix js2 to actually load for js files

--- emacs/modules/abn-module-javascript.el
+++ emacs/modules/abn-module-javascript.el
@@ -9,15 +9,11 @@
 
 (use-package abn-funcs-javascript
   :ensure nil ; local package
-  :commands (abn//javascript-extend-jsdoc-tags)
-  :general
-  (abn/define-leader-keys
-   "fem" 'abn/new-module)
-  )
+  :commands (abn//javascript-extend-jsdoc-tags))
 
 (use-package js2-mode
   :defer t
-  :diminish '("Javascript-IDE" . "JS")
+  :mode ("\\.js\\'" . js2-mode)
   :general
   (:keymaps 'js2-mode-map
    "fs" 'abn/javascript-format-js-code-in-string
@@ -31,7 +27,11 @@
   (setq-default js2-basic-offset 2)
   (setq-default js2-strict-trailing-comma-warning nil)
   (setq-default js2-mode-show-strict-warnings nil)
-  (abn//javascript-extend-jsdoc-tags))
+  (abn//javascript-extend-jsdoc-tags)
+
+  (defun abn/shorten-js2-mode-name ()
+    (setq mode-name "JS"))
+  (add-hook 'js2-mode-hook #'abn/shorten-js2-mode-name))
 
 (provide 'abn-module-javascript)
 ;;; abn-module-javascript.el ends here
