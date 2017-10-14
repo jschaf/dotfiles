e785c5d5b9dc4d41c72865f0d7c2e8e130b98d73
Joe Schafer <joe@jschaf.com>
2017-10-14 00:12:46 -0700

    emacs: remove more code from old spacemacs config

--- layers/joe/config.el
+++ layers/joe/config.el
@@ -6,50 +6,6 @@
 
 ;;; Code:
 
-(when (eq system-type 'darwin)
-  (defvar mac-option-modifier)
-  (defvar mac-command-modifier)
-
-  (setq mac-option-modifier 'meta
-        mac-command-modifier 'super)
-
-  (defvar my:mac-modifier-state 'mac
-    "Toggle between BUILT-IN and USB.")
-
-  (defun my:toggle-mac-modifiers ()
-    (interactive)
-    (if (eq my:mac-modifier-state 'usb)
-        (progn
-          (setq my:mac-modifier-state 'built-in
-                mac-option-modifier 'control
-                mac-command-modifier 'meta)
-          (message "Mac modifier keys set for Mac keyboard."))
-
-      (setq my:mac-modifier-state 'usb
-            mac-option-modifier 'meta
-            mac-command-modifier 'super)
-      (message "Mac modifier keys set for USB keyboard.")))
-
-  (setq my:mac-modifier-state 'built-in
-        mac-option-modifier 'control
-        mac-command-modifier 'meta)
-  (message "Mac modifier keys set for Mac keyboard."))
-
-(defun my:back-to-indentation-or-beginning ()
-  "Move point to first non-whitespace char or `beginning-of-line'."
-  (interactive)
-  (let ((oldpos (point)))
-    (back-to-indentation)
-    (and (= oldpos (point))
-         (beginning-of-line))))
-
-(defun unfill-paragraph ()
-  "Replace newline characters with a single space."
-  (interactive)
-  (let ((fill-column 90002000))
-    (if mark-active
-        (fill-region (region-beginning) (region-end))
-      (fill-paragraph))))
 
 (defun my:new-blah-buffer ()
   "Open up a guaranteed new blah (scratch) buffer."
@@ -88,20 +44,6 @@ non-nil, keep the original buffer content."
            return (switch-to-buffer buffer)
            finally do (my:new-blah-buffer)))
 
-(setq-default evil-escape-key-sequence "jk")
-
-(setq-default sentence-end-double-space t)
-
-(defun my:nuke-all-buffers ()
-  "Kill all buffers, leaving only *scratch* only."
-  (interactive)
-  (mapc
-   (lambda (buffer)
-     (kill-buffer buffer))
-   (buffer-list))
-  (delete-other-windows))
-
-
 ;; (with-eval-after-load 'lisp-mode
 ;;   (require 'pp)
 ;;   (defun my-pp-eval-last-sexp-in-current-buffer ()
@@ -110,16 +52,6 @@ non-nil, keep the original buffer content."
 ;;   (spacemacs/set-leader-keys-for-major-mode 'lisp-interaction-mode
 ;;     "ep" 'my-pp-eval-last-sexp-in-current-buffer
 ;;     "eP" 'pp-eval-last-sexp))
-
-;; I always hit this by mistake to get to `describe-char' and I'm tired of
-;; seeing the GNU license
-(global-set-key (kbd "C-h C-c") 'describe-key-briefly)
-
-;; UTF-8 everywhere
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
 
 ;; .dir-local.el tweaks
 ;;
@@ -146,84 +78,6 @@ Primarily for use in .dir-locals.el")
 
 ;; Custom keymaps
 (defvar joe-map (make-keymap))
-
-(defun joe/set-leader-keys (key def &rest bindings)
-  "Add KEY and DEF as key bindings under `joe-map'.
-The key for `joe-map' is `joe-leader-key'.  KEY should be a
-string suitable for passing to `kbd', and it should not include
-the leaders.  DEF is most likely a quoted command.  See
-`define-key' for more information about the possible choices for
-DEF.  This function simply uses `define-key' to add the bindings.
-
-For convenience, BINDINGS is additional KEY DEF pairs.  For
-example,
-
-\(joe/set-leader-keys
-   \"a\" 'command1
-   \"c\" 'command2
-   \"bb\" 'command3\)"
-  (while key
-    (define-key joe-map (kbd key) def)
-    (setq key (pop bindings) def (pop bindings))))
-
-(spacemacs/set-leader-keys
-  "," joe-map)
-
-(defun my:find-file-builder (name path)
-  "Create function to open PATH."
-  `(defun ,name()
-     (interactive)
-     (find-file ,path)))
-
-(defun my:make-file-shortcuts-in-dir (dir bindings)
-  "Create shortcuts to DIR for file BINDINGS.
-DIR is a root directory path.  BINDINGS is a list of 3-element
-lists, with a binding shortcut, a symbol for the function name
-and the file path relative to DIR."
-  (declare (indent 1))
-  (cl-loop for (binding function-name path) in bindings
-           do
-           (let ((file-finder-defun (my:find-file-builder
-                                     function-name (concat dir path))))
-             (eval file-finder-defun)
-             (joe/set-leader-keys binding function-name))))
-
-(my:make-file-shortcuts-in-dir "~/.dotfiles/"
-  '(("fdc" my:open-joe-config "layers/joe/config.el")
-    ("fdi" my:open-i3-conf "tag-linux/config/i3/config")
-    ("fdm" my:open-my-org "layers/joe/local/my-org.el")
-    ("fdp" my:open-joe-packages "layers/joe/packages.el")
-    ("fdt" my:open-tmux-conf "tmux.conf")
-    ("fdT" my:open-joe-tlp "layers/joe/local/tlp/tlp.el")
-    ("fdx" my:open-joe-xinitrc "tag-linux/xsession")
-    ("fdzz" my:open-zshrc "zsh/.zshrc")
-    ("fdzk" my:open-zshrc-keys "zsh/keys.zsh")
-    ("fdzp" my:open-zsh-plugins "zsh/plugins.zsh")
-    ("fdze" my:open-zshenv "zsh/.zshenv")
-    ("fdzr" my:open-zsh-prompts "zsh/prompts/prompt_pure_setup")
-    ))
-
-(my:make-file-shortcuts-in-dir "~/.zsh/"
-  '(("fdzh" my:open-zsh-host "host.zsh")
-    )
-  )
-(my:make-file-shortcuts-in-dir "~/.dotfiles-work/"
-  '(("fdzw" my:open-zsh-work "zsh/work.zsh")
-    ("fdhr" my:open-hgrc "hgrc")
-    ))
-
-(my:make-file-shortcuts-in-dir "~/gdrive/"
-  '(("fgg" my:open-gtd "org/gtd.org")
-    ("fgw" my:open-goog "gorg/goog.org")
-    ("fgj" my:open-journal "org/journal.org")
-    ("fgs" my:open-sandlot "gorg/sandlot.org")
-    ("fgr" my:open-refile "org/refile.org")
-    ("fgt" my:open-refile "gorg/today.org")
-    ("fgp" my:open-people "org/people.org")
-    ("fgl" my:open-ledger "financials/personal.ledger")
-    ("fgc" my:open-joe-checklist "org/checklists/checklist.org")
-    ("fgR" my:open-refile-work "org/work-refile.org")
-    ("fgd" my:open-org-drill "drill/programming.org")))
 
 (defun operate-on-point-or-region (fn)
   "Get the current unspaced string at point.
@@ -272,76 +126,6 @@ The return value is ELEM.
       (goto-char (match-end 0))
       (backward-char 6) (insert "\n") (incf end))
     (indent-region begin end nil)))
-
-(with-eval-after-load 's
-  (defun my:snake-case-at-point-or-region ()
-    "Snake_case the current word or text selection."
-    (interactive)
-    (operate-on-point-or-region 's-snake-case))
-
-  (defun my:dasherise-at-point-or-region ()
-    "Dasherise-the-current CamelCase or snake_case word or text selection."
-    (interactive)
-    (operate-on-point-or-region 's-dashed-words))
-
-  (defun my:upper-camelcase-at-point-or-region ()
-    "UpperCamelCaseTheCurrent dashed-or-snake_case_words or any words in text selection."
-    (interactive)
-    (operate-on-point-or-region 's-upper-camel-case))
-
-  (defun my:lower-camelcase-at-point-or-region ()
-    "LowerCamelCaseTheCurrent dashed or snake_case word or any words in text selection."
-    (interactive)
-    (operate-on-point-or-region 's-lower-camel-case))
-
-  (defun my:humanize-at-point-or-region ()
-    "Humanize variable names, insert spaces instead of - or _ or un-CamelCase humps to spaced words."
-    (interactive)
-    (operate-on-point-or-region 's-capitalized-words))
-
-  (defun my:titleized-at-point-or-region ()
-    "Convert snaked, dashed, underscored, camelcase, or spaced words in region to Title Case."
-    (interactive)
-    (operate-on-point-or-region 's-titleized-words))
-
-  (defun my:flush-blank-lines ()
-    "Flush blank lines."
-    (interactive)
-    (flush-lines "^\s*$" nil nil t))
-
-  (joe/set-leader-keys
-   "bb" 'my:switch-to-blah-buffer
-   "bB" 'my:new-blah-buffer
-   "nb" 'my:nuke-all-buffers
-   "ss" 'my:snake-case-at-point-or-region
-   "sd" 'my:dasherise-at-point-or-region
-   "scu" 'my:upper-camelcase-at-point-or-region
-   "scl" 'my:lower-camelcase-at-point-or-region
-   "sh" 'my:humanize-at-point-or-region
-   "st" 'my:titleized-at-point-or-region
-   "tm" 'my:toggle-mac-modifiers
-   "xf" 'my:flush-blank-lines))
-
-(defun my:insert-newline-above-and-follow ()
-  (interactive)
-  (beginning-of-line)
-  (insert "\n")
-  (forward-line -1))
-
-(defun my:insert-newline-below-and-follow ()
-  (interactive)
-  (end-of-line)
-  (insert "\n"))
-
-(defun my:insert-newline-above-and-stay ()
-  (interactive)
-  (save-excursion
-    (my:insert-newline-above-and-follow)))
-
-(defun my:insert-newline-below-and-stay ()
-  (interactive)
-  (save-excursion (my:insert-newline-below-and-follow)))
-
 
 (evil-leader/set-key "iSr" 'yas-reload-all)
 
