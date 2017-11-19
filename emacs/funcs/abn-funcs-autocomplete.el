e8bef2e89a32a51ace3197ed90459b2a83a4cfcf
Joe Schafer <joe@jschaf.com>
2017-11-19 13:35:21 -0800

    emacs: fix bugs in else template

--- emacs/funcs/abn-funcs-autocomplete.el
+++ emacs/funcs/abn-funcs-autocomplete.el
@@ -11,15 +11,19 @@
 
 (defvar abn--else-mappings
   '(("ELSE-Template" . "Template")
-    ("Elisp" . "Emacs-Lisp"))
+    ("Elisp" . "Emacs-Lisp")
+    ;; ("Shell-script[zsh]" . "Shell")
+    ("sh" . "Shell")
+    )
   "Mapping from a major mode to an ELSE template language.")
 
-(defun abn//setup-else-mappings (alist)
+(defun abn//setup-else-mappings ()
   "Set up mapping from major mode names to ELSE template names."
   (cl-loop for (new-name . old-name) in abn--else-mappings
            do
-           (unless (assoc key else-Alternate-Mode-Names)
-             (add-to-list 'else-Alternate-Mode-Names (cons new-name old-name)))))
+           (unless (assoc new-name else-Alternate-Mode-Names)
+             (add-to-list 'else-Alternate-Mode-Names (cons new-name old-name))))
+  abn--else-mappings)
 
 (provide 'abn-funcs-autocomplete)
 ;;; abn-funcs-autocomplete.el ends here
