9b1fb50567ac17925d6b7eb5695e158a24b64227
Joe Schafer <joe@jschaf.com>
2017-11-19 15:42:37 -0800

    emacs: add abn/else-next-if-nearby

--- emacs/funcs/abn-funcs-autocomplete.el
+++ emacs/funcs/abn-funcs-autocomplete.el
@@ -25,5 +25,27 @@
              (add-to-list 'else-Alternate-Mode-Names (cons new-name old-name))))
   abn--else-mappings)
 
+(defun abn/else-kill-always ()
+  "Like `else-kill' but kills required placeholders without prompting."
+  (interactive)
+  (else-kill 'force))
+
+
+(defvar abn--else-next-placeholder-threshold 5
+  "Goto the next placeholder after a kill if less than n lines away.")
+
+(defun abn/else-next-if-nearby ()
+  "Proceed to next placeholder if near enough to the current point."
+  (interactive)
+  (let* ((here (current-point))
+         (here-line (line-number-at-pos here))
+         (next-point (progn (else-next 1 :no-error-msg t)
+                            (point)))
+         (next-line (line-number-at-pos next-point)))
+    ;; The point was moved in `else-next'.  Only go back if we exceed
+    ;; the threshold.
+    (when (> (- next-line here-line) abn--else-next-placeholder-threshold)
+      (goto-char here))))
+
 (provide 'abn-funcs-autocomplete)
 ;;; abn-funcs-autocomplete.el ends here
