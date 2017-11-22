faf7ac1085ee94abfb31a1efb0988466eaedf19a
Joe Schafer <joe@jschaf.com>
2017-11-22 13:50:35 -0500

    emacs: add SPC to abn-leader-map in customize

--- emacs/modules/abn-module-help.el
+++ emacs/modules/abn-module-help.el
@@ -14,5 +14,12 @@
   (abn/define-leader-keys
    "hdK" 'abn/describe-keymap))
 
+(use-package cust-edit
+  :defer t
+  :ensure nil ; built-in package
+  :general
+  (:keymaps 'custom-mode-map
+   "SPC" abn-leader-map))
+
 (provide 'abn-module-help)
 ;;; abn-module-help.el ends here
