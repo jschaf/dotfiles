d1ad193038fdf86e4a8bfecdcd185d487ab8b61c
Joe Schafer <joe@jschaf.com>
2017-12-16 00:02:44 -0800

    emacs: remove quelpa

--- emacs/core/abn-core-packages.el
+++ emacs/core/abn-core-packages.el
@@ -17,15 +17,6 @@
 
 (package-initialize)
 
-(defun abn-install-quelpa ()
-  "Install `quelpa'.
-Typically needed to pin specific library commits."
-  (unless (require 'quelpa nil t)
-    (with-temp-buffer
-      (url-insert-file-contents
-       "https://raw.github.com/quelpa/quelpa/master/bootstrap.el")
-      (eval-buffer))))
-
 (defun abn-install-use-package ()
   "Install `use-package'."
   (unless (package-installed-p 'use-package)
@@ -38,18 +29,6 @@ Typically needed to pin specific library commits."
 
 (require 'use-package)
 (setq use-package-always-ensure t)
-
-(use-package quelpa-use-package
-  :defer nil ; load immediately
-  :init
-  (require 'quelpa-use-package))
-
-;; TODO: Replace with use-package after
-;; https://github.com/noctuid/general.el/issues/90 is fixed.
-(abn-install-quelpa)
-(quelpa
- '(general :fetcher github :repo "noctuid/general.el"
-           :commit "a0762be9fffc84acffe3be6678c9b7d8f13f1898"))
 
 (use-package which-key
   :defer nil ; load immediately
@@ -89,7 +68,6 @@ Typically needed to pin specific library commits."
 
 (define-key package-menu-mode-map "s" #'abn-package-menu-filter-by-status)
 (define-key package-menu-mode-map "a" #'abn-package-menu-find-marks)
-
 
 (provide 'abn-core-packages)
 ;;; abn-core-packages.el ends here
