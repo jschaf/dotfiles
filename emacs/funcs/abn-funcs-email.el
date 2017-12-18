9ae07d57f56b074185082123eba1e1c7f6a942b0
Joe Schafer <joe@jschaf.com>
2017-12-18 02:34:25 -0800

    emacs: set up initial email

--- emacs/funcs/abn-funcs-email.el
+++ emacs/funcs/abn-funcs-email.el
@@ -5,5 +5,24 @@
 
 ;;; Code:
 
+(defun abn/mu4e-set-account ()
+  "Set the account for composing a message."
+  (let* ((account
+          (if mu4e-compose-parent-message
+              (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
+                (string-match "/\\(.*?\\)/" maildir)
+                (match-string 1 maildir))
+            (completing-read (format "Compose with account: (%s) "
+                                     (mapconcat #'(lambda (var) (car var))
+                                                abn-mu4e-account-alist "/"))
+                             (mapcar #'(lambda (var) (car var)) abn-mu4e-account-alist)
+                             nil t nil nil (caar abn-mu4e-account-alist))))
+         (account-vars (cdr (assoc account abn-mu4e-account-alist))))
+    (if account-vars
+        (mapc #'(lambda (var)
+                  (set (car var) (cadr var)))
+              account-vars)
+      (error "No email account found"))))
+
 (provide 'abn-funcs-email)
 ;;; abn-funcs-email.el ends here
--- emacs/modules/abn-module-email.el
+++ emacs/modules/abn-module-email.el
@@ -9,7 +9,71 @@
 
 (use-package abn-funcs-email
   :ensure nil ; local package
+  :commands (abn/mu4e-set-account)
   )
+
+(use-package mu4e
+  :ensure nil ; Installed with package manager
+  :defer t
+  :config
+
+  ;; Set inital value.  Will be overridden by `mu4e-account-alist'.
+  (setq
+   mu4e-maildir       "~/mail"
+   ;; Folder names relative to `mu4e-maildir'.
+   mu4e-sent-folder   "/joesmoe10/sent"
+   mu4e-drafts-folder "/joesmoe10/drafts"
+   mu4e-trash-folder  "/joesmoe10/trash"
+   mu4e-refile-folder "/joesmoe10/archive")
+
+
+  (setq abn-mu4e-account-alist
+        '(("joesmoe10"
+           ;; User info.
+           (user-full-name "Joe Schafer")
+           (user-mail-address "joesmoe10@gmail.com")
+           (mu4e-compose-signature "--\nJoe Schafer")
+
+           ;; Account-specific variables.
+           (mu4e-sent-messages-behavior delete)
+           (mu4e-sent-folder "/joesmoe10/sent")
+           (mu4e-refile-folder "/joesmoe10/archive")
+           (mu4e-drafts-folder "/joesmoe10/drafts")
+
+           ;; SMTP
+           (smtpmail-stream-type starttls)
+           (smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil)))
+           (smtpmail-smtp-user "joesmoe10")
+           (smtpmail-smtp-server "smtp.gmail.com")
+           (smtpmail-smtp-service 587))
+
+          ("delta46"
+           ;; User info.
+           (user-full-name "Joe Schafer")
+           (user-mail-address "joe.schafer@delta46.us")
+           (mu4e-compose-signature "--\nJoe Schafer")
+
+           ;; Account-specific variables.
+           (mu4e-sent-messages-behavior delete)
+           (mu4e-sent-folder "/delta46/sent")
+           (mu4e-refile-folder "/delta46/archive")
+           (mu4e-drafts-folder "/delta46/drafts")
+
+           ;; SMTP
+           (smtpmail-stream-type starttls)
+           (smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil)))
+           (smtpmail-smtp-user "joe.schafer@delta46.us")
+           (smtpmail-smtp-server "smtp.gmail.com")
+           (smtpmail-smtp-service 587))))
+
+
+  ;;send mail using postfix
+  (setq send-mail-function 'sendmail-send-it)
+  (setq message-send-mail-function 'message-send-mail-with-sendmail)
+
+  (setq mu4e-change-filenames-when-moving t)
+  (add-hook 'mu4e-compose-pre-hook 'abn/mu4e-set-account))
+
 
 
 
