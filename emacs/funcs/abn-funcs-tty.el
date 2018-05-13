587f5e1973be8163f2eebc37fc326b32023e7ddb
Joe Schafer <jschaf@google.com>
2018-05-13 02:58:34 -0700

    emacs: enable osc52 in tty mode

--- emacs/funcs/abn-funcs-tty.el
+++ emacs/funcs/abn-funcs-tty.el
@@ -5,5 +5,24 @@
 
 ;;; Code:
 
+(defun abn-tty/yank (text)
+  "Yank TEXT to the clipboard from a terminal Emacs."
+  (if window-system
+      (error "Trying to copy text in a GUI emacs.")
+    (with-temp-buffer
+      (insert text)
+      (call-process-region (point-min) (point-max)
+                           "~/.dotfiles/bin/clipboard-copy"))))
+
+(defun abn-tty/buffer-substring-terminal-filter (beg end &optional delete)
+  "A filter that uses the default filter but also adds text to clipboard."
+  (let ((result (buffer-substring--filter beg end delete)))
+    ;; Only copy sizable entries to avoid spamming the clipboard.
+    (when (> (length result) 4)
+      (osc52-select-text (buffer-substring beg end))
+      ;; (abn-tty/yank result)
+      )
+    result))
+
 (provide 'abn-funcs-tty)
 ;;; abn-funcs-tty.el ends here
--- emacs/modules/abn-module-tty.el
+++ emacs/modules/abn-module-tty.el
@@ -7,6 +7,12 @@
 (eval-when-compile
   (require 'use-package))
 
+(use-package abn-funcs-tty
+  :ensure nil ; local package
+  :commands
+  (abn-tty/yank
+   abn-tty/buffer-substring-terminal-filter))
+
 ;; Change the cursor display in a terminal emacs.
 (use-package evil-terminal-cursor-changer
   :if (not (display-graphic-p))
@@ -16,16 +22,23 @@
 	evil-insert-state-cursor 'bar
 	evil-emacs-state-cursor 'hbar)
   :config
-  (evil-terminal-cursor-changer-activate))
+  (evil-terminal-cursor-changer-activate)
 
-(use-package abn-funcs-tty
+  (setq-default filter-buffer-substring-function
+                ;; #'buffer-substring--filter
+                #'abn-tty/buffer-substring-terminal-filter
+                ))
+
+;; Commands to send text to the host terminal's clipboard.
+(use-package osc52
   :ensure nil ; local package
-  )
+  :commands
+  (osc52-select-text))
 
 ;; The inactive background is indistinguishable from the normal
 ;; background.
-(set-face-background 'mode-line "brightmagenta")
-(set-face-background 'mode-line-inactive "black")
+(set-face-background 'mode-line "#303030")
+(set-face-background 'mode-line-inactive "#1c1c1c")
 
 (provide 'abn-module-tty)
 ;;; abn-module-tty.el ends here
