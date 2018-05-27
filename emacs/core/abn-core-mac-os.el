9424749a7e2f690786c03bf118ad2abec1f364ac
Joe Schafer <joe@jschaf.com>
2018-05-27 15:30:26 -0700

    emacs: add browse-url-generic-program to "open" for mac

--- emacs/core/abn-core-mac-os.el
+++ emacs/core/abn-core-mac-os.el
@@ -9,7 +9,13 @@
 (setq mac-option-modifier 'control
       mac-command-modifier 'meta)
 
-(setq browse-url-browser-function 'browse-url-default-macosx-browser)
+(use-package browse-url
+  :defer t
+  :ensure nil ; built-in package
+  :config
+  (setq browse-url-generic-program "open")
+  (setq browse-url-browser-function 'browse-url-default-macosx-browser))
+
 
 (provide 'abn-core-mac-os)
 ;;; abn-core-mac-os.el ends here
