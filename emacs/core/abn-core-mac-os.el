7d0b2c39740e72b21e3b7c08c80748594a1e7d3a
Joe Schafer <joe@jschaf.com>
2019-03-18 14:55:08 -0700

    emacs: add MacOS function to swap meta and command keys
    Make one suitable for the built-in keyboard and one for external
    keyboards.

--- emacs/core/abn-core-mac-os.el
+++ emacs/core/abn-core-mac-os.el
@@ -6,8 +6,21 @@
 
 ;;; Code:
 
+;; Suitable defaults for the Macbook pro keyboard
 (setq mac-option-modifier 'control
       mac-command-modifier 'meta)
+
+(defun abn/toggle-mac-keyboard ()
+  "Switch between keyboard controls for an external keyboard and built-in keyboard"
+  (interactive)
+  (if (eq mac-option-modifier 'control)
+      (progn
+        (setq mac-option-modifier 'meta
+              mac-command-modifier 'control)
+        (message "Set MacOs keybindings for external keyboard."))
+    (setq mac-option-modifier 'control
+          mac-command-modifier 'meta)
+    (message "Set MacOs keybindings for built-in keyboard.")))
 
 (use-package browse-url
   :defer t
